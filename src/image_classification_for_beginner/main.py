00fd89b297eb369c4976fe85a8384e1d4b12f424
Oleksandr Pronin <pronin.alx@gmail.com>
2024-02-04 17:16:07 +0100

    feat: count the number of images of each rice category

--- src/image_classification_for_beginner/main.py
+++ src/image_classification_for_beginner/main.py
@@ -1,5 +1,18 @@
+import pandas as pd
+import os
+
+
 def main():
-    print('Welcome to main!')
+    img_path = "data/Rice_Image_Dataset/"
+    rice_label = ['Arborio', 'Basmati', 'Ipsala', 'Jasmine', 'Karacadag']
+    img_list = []
+    label_list = []
+    for label in rice_label:
+        for img_file in os.listdir(img_path+label):
+            img_list.append(img_path+label+'/'+img_file)
+            label_list.append(label)
+    df = pd.DataFrame({'img': img_list, 'label': label_list})
+    print(df['label'].value_counts())
 
 
 if __name__ == "__main__":
