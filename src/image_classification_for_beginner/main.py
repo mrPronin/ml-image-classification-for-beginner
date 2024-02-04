336dcdd6764d79d6a8db223320f256aa116eca71
Oleksandr Pronin <pronin.alx@gmail.com>
2024-02-04 18:56:16 +0100

    feat: show sample images

--- src/image_classification_for_beginner/main.py
+++ src/image_classification_for_beginner/main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 
 def main():
     img_path = "data/Rice_Image_Dataset/"
+
     rice_label = ['Arborio', 'Basmati', 'Ipsala', 'Jasmine', 'Karacadag']
+    # create a dataframe of image path and label
     img_list = []
     label_list = []
     for label in rice_label:
@@ -12,7 +15,19 @@ def main():
             img_list.append(img_path+label+'/'+img_file)
             label_list.append(label)
     df = pd.DataFrame({'img': img_list, 'label': label_list})
+
+    # count the number of images of each rice category
     print(df['label'].value_counts())
+
+    # show sample images
+    fig, ax = plt.subplots(ncols=len(rice_label), figsize=(20, 4))
+    fig.suptitle('Rice Category')
+    random_num = 12
+    for i, label in enumerate(rice_label):
+        path = df[df['label'] == label]['img'].iloc[random_num]
+        # print(f"i: {i} label: {label} path: {path}")
+        ax[i].set_title(label)
+        ax[i].imshow(plt.imread(path))
 
 
 if __name__ == "__main__":
